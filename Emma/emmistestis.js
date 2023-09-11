d943327b8924a84f0156902371a4e5e778f2e5e7
emmanv232 <emma.andersson_@hotmail.com>
2023-09-11 08:51:26 +0200

    Emmisens skit

---
+++ Emma/emmistestis.js
@@ -0,0 +1,15 @@
+const popup = () => { 
+    const popups = document.querySelectorAll(".popup");
+    for (let i = 0; i < popups.length; i++) {
+    const windowHeight = window.innerHeight;
+    const elementTop = popups[i].getBoundingClientRect().top;
+    const elementVisible = 150; 
+    if (elementTop < windowHeight - elementVisible) {
+    popups[i].classList.add("active");
+    } 
+    else { 
+    popups[i].classList.remove("active");
+    } 
+    } 
+   } 
+   window.addEventListener("scroll", popup);
