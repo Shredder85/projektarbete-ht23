b5d9f43887e09bbef7e6312e208af25c55264962
Axel Österlund <132377178+AxelOsterlund1@users.noreply.github.com>
2023-09-12 16:12:15 +0200

    Update för tjänster

--- Axel/axel.js
+++ Axel/axel.js
@@ -1,3 +1,22 @@
+import {createApp} from 'https://unpkg.com/petite-vue?module';
+
+const app = createApp({
+products:[
+{name: 'System', icon: 'images/it-systems (1).png', description: 'Design och hantering av hård- och mjukvarusystem för effektiv datahantering.' 
+, price:'1337$'},
+{name: 'Devops', icon:'images/devops.png', description: 'Automatisering av mjukvaruutveckling och driftsprocesser för snabbare och kontinuerlig mjukvaruleverans.' 
+, price:'1337$'},
+{name: 'Security', icon:'images/elearning.png', description: 'Skydd av digitala tillgångar och data mot cyberhot och incidenthantering.' 
+, price:'1337$'},
+{name: 'Software', icon:'images/software.png', description: 'Utveckling och underhåll av skräddarsydda programvarulösningar för specifika företagsbehov.' 
+, price:'1337$'},
+]
+
+});
+app.mount('#content_services');
+
+
+
 const popup = () => { 
     const popups = document.querySelectorAll(".popup");
     for (let i = 0; i < popups.length; i++) {
