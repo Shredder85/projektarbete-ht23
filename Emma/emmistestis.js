be722ec07cbc51401bc8edc779da0a40a8012db2
emmanv232 <emma.andersson_@hotmail.com>
2023-09-12 09:40:34 +0200

    totalis fuckeri

--- Emma/emmistestis.js
+++ Emma/emmistestis.js
@@ -13,3 +13,19 @@ const popup = () => {
     } 
    } 
    window.addEventListener("scroll", popup);
+
+   const popup1 = () => { 
+    const popups = document.querySelectorAll(".popup1");
+    for (let i = 0; i < popups.length; i++) {
+    const windowHeight = window.innerHeight;
+    const elementTop = popups[i].getBoundingClientRect().top;
+    const elementVisible = 100; 
+    if (elementTop < windowHeight - elementVisible) {
+    popups[i].classList.add("active1");
+    } 
+    else { 
+    popups[i].classList.remove("active1");
+    } 
+    } 
+   } 
+   window.addEventListener("scroll", popup1);
